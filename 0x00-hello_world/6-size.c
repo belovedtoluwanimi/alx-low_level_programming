498fa7d7df93e1547bc16e1d4d6150f4423450af
belovedtoluwanimi <belovedtoluwanimi16@gmail.com>
2022-10-13 05:43:57 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types,
- * print sizes
+ * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
