6673bfe0771f5ee35e8f7c05becc0e50a58ce6a9
belovedtoluwanimi <belovedtoluwanimi16@gmail.com>
2022-10-13 02:38:12 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints a text
  * 
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
